88d590a5db5d0ee1f72e61d0f49e30b31c9d5101
Hazz-i <wahidh697@gmail.com>
2024-05-18 18:09:33 +0700

    add: responsif profile

--- FrontEnd/src/Pages/Member/Index.tsx
+++ FrontEnd/src/Pages/Member/Index.tsx
@@ -42,7 +42,7 @@ const Member = () => {
             {/* END TOP CONTENT */}
 
             {/* CONTENT */}
-            <div className="flex bg-gray-100 rounded-t-lg flex-col pt-28 px-5 gap-5 lg:h-screen lg:pt-[6rem] lg:ps-[10rem] lg:gap-0 xl:ps-[12rem] xl:pe-[2rem] xl:flex-row xl:gap-5">
+            <div className="flex bg-gray-100 rounded-t-lg flex-col pt-28 pb-4 gap-5 lg:h-screen lg:pt-[6rem] lg:ps-[10rem] lg:gap-0 xl:ps-[12rem] xl:pe-[2rem] xl:flex-row xl:gap-5">
                 <div className="flex flex-col xl:w-[56rem] ">
                     {/* CENTER CUBE */}
                     <div className="absolute flex items-center justify-start top-64 gap-5 w-full overflow-x-scroll ps-5 pb-5 md:justify-center md:ps-0 lg:relative lg:top-0 lg:pb-5 lg:justify-start lg:mx-10">
@@ -60,7 +60,7 @@ const Member = () => {
                     </div>
                     {/* END CENTER CUBE */}
 
-                    <div className="flex gap-5 flex-col md:flex-row md:mx-10 lg:pb-5 lg:ms-10 lg:mx-0">
+                    <div className="flex gap-5 px-5 flex-col md:flex-row md:mx-10 lg:pb-5 lg:ms-10 lg:mx-0">
                         {/* PAPAN INFO */}
                         <HomeCard title="PAPAN INFO">
                             <span className="flex flex-col w-1/2 justify-center items-center">
@@ -106,53 +106,57 @@ const Member = () => {
 
                 {/* XL CONTENT HIDDEN */}
                 {/* LAST ACTIVITY */}
-                <span className="w-full bg-white rounded-xl py-7 mx-auto flex flex-col shadow-md gap-5 mb-36 px-1 md:mx-10 md:w-auto lg:py-0 lg:overflow-hidden lg:px-0 lg:gap-0 xl:hidden">
-                    {/* HEADER */}
-                    <div className="flex justify-between items-center px-8 lg:bg-purple-600 lg:py-5 lg:text-white">
-                        <h1 className="font-semibold">Last Activity</h1>
-                        <button className=" flex items-center justify-center text-purple-600 lg:text-white">
-                            show all{" "}
-                            <i className="bx bx-chevron-right text-gray-400 lg:text-white"></i>
-                        </button>
-                    </div>
-                    {/* END HEADER */}
-                    <hr className="lg:hidden" />
-                    {/* CONTENT */}
-                    <ul>
-                        <li className="flex justify-between items-center  hover:bg-gray-300 cursor-pointer px-8 py-2 rounded-lg">
-                            {/* LEFT */}
-                            <span className="flex gap-1">
-                                <div className="flex gap-4">
-                                    <button className="w-14 h-14 rounded-full border overflow-hidden flex items-center justify-center">
-                                        <img
-                                            src="https://i.pinimg.com/236x/d7/68/42/d76842da733b7e4a2c679c0a6d0ba75e.jpg"
-                                            alt=""
-                                        />
-                                    </button>
-                                    <span className="flex items-start justify-center flex-col">
-                                        <h1 className="font-semibold">
-                                            Listrik
-                                        </h1>
-                                        <small className="text-gray-400">
-                                            KAS
-                                        </small>
-                                    </span>
-                                </div>
-                            </span>
-                            {/* END LEFT */}
+                <div className="px-5">
+                    <span className="w-full bg-white rounded-xl py-7 mx-auto flex flex-col shadow-md gap-5 mb-36 px-1 md:mx-10 md:w-auto lg:py-0 lg:overflow-hidden lg:px-0 lg:gap-0 xl:hidden">
+                        {/* HEADER */}
+                        <div className="flex justify-between items-center px-8 lg:bg-purple-600 lg:py-5 lg:text-white">
+                            <h1 className="font-semibold">Last Activity</h1>
+                            <button className=" flex items-center justify-center text-purple-600 lg:text-white">
+                                show all{" "}
+                                <i className="bx bx-chevron-right text-gray-400 lg:text-white"></i>
+                            </button>
+                        </div>
+                        {/* END HEADER */}
+                        <hr className="lg:hidden" />
+                        {/* CONTENT */}
+                        <ul>
+                            <li className="flex justify-between items-center  hover:bg-gray-300 cursor-pointer px-8 py-2 rounded-lg">
+                                {/* LEFT */}
+                                <span className="flex gap-1">
+                                    <div className="flex gap-4">
+                                        <button className="w-14 h-14 rounded-full border overflow-hidden flex items-center justify-center">
+                                            <img
+                                                src="https://i.pinimg.com/236x/d7/68/42/d76842da733b7e4a2c679c0a6d0ba75e.jpg"
+                                                alt=""
+                                            />
+                                        </button>
+                                        <span className="flex items-start justify-center flex-col">
+                                            <h1 className="font-semibold">
+                                                Listrik
+                                            </h1>
+                                            <small className="text-gray-400">
+                                                KAS
+                                            </small>
+                                        </span>
+                                    </div>
+                                </span>
+                                {/* END LEFT */}
 
-                            {/* RIGHT */}
-                            <span className="flex flex-col">
-                                <small className="font-semibold">
-                                    Rp 50.000
-                                </small>
-                                <small className="text-gray-400">Today</small>
-                            </span>
-                            {/* END RIGHT */}
-                        </li>
-                    </ul>
-                    {/* END CONTENT */}
-                </span>
+                                {/* RIGHT */}
+                                <span className="flex flex-col">
+                                    <small className="font-semibold">
+                                        Rp 50.000
+                                    </small>
+                                    <small className="text-gray-400">
+                                        Today
+                                    </small>
+                                </span>
+                                {/* END RIGHT */}
+                            </li>
+                        </ul>
+                        {/* END CONTENT */}
+                    </span>
+                </div>
                 {/* END LAST ACTIVITY */}
 
                 {/* JUST SHOW ON XL+ WIDTH */}
